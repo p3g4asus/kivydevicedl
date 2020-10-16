28075a17484d03196261fa10b71af8e84b314967
Matteo Federico Zazzetta <fulminedipegasus@gmail.con>
2020-10-17 01:00:29 +0200

    Upnpirta2 device support

---
+++ src/java_classes/org/kivymfz/devicedl/mqtt/device/DeviceUpnpirta2.java
@@ -0,0 +1,9 @@
+package org.kivymfz.devicedl.mqtt.device;
+
+import com.hivemq.client.mqtt.mqtt3.message.publish.Mqtt3Publish;
+
+public class DeviceUpnpirta2 extends DeviceRm {
+    public DeviceUpnpirta2(String name, Mqtt3Publish pub) {
+        super(name, pub);
+    }
+}
