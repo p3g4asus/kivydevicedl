50dd876b32577468733b8ffc0a1f7434703e0905
Matteo Federico Zazzetta <fulminedipegasus@gmail.com>
2021-04-19 20:34:44 +0200

    Forgot import

--- src/java_classes/org/kivymfz/devicedl/mqtt/device/DeviceTasmotaswitch.java
+++ src/java_classes/org/kivymfz/devicedl/mqtt/device/DeviceTasmotaswitch.java
@@ -1,5 +1,7 @@
 package org.kivymfz.devicedl.mqtt.device;
 
+import com.hivemq.client.mqtt.mqtt3.message.publish.Mqtt3Publish;
+
 public class DeviceTasmotaswitch extends DeviceS20 {
 
     public DeviceTasmotaswitch(String name, Mqtt3Publish pub) {
